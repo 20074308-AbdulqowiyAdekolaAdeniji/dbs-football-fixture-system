43d2b268b49c439739878ad7a9eaf2fa88f27e28
Abdulqowiy Adeniji <20074308@mydbs.ie>
2025-11-17 18:43:08 +0000

    Created basic FixtureForm component structure with a README

---
+++ frontend/src/components/FixtureForm.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+function FixtureForm() {
+  return (
+    <div className="fixture-form">
+      <h2>Add New Fixture</h2>
+      <form>
+        <p>Form fields will be added here</p>
+      </form>
+    </div>
+  );
+}
+
+export default FixtureForm;
